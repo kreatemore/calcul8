e0db40a7aad3a42009fdeac8b6246c224f095a09
Alex Szabó <kreatemore@gmail.com>
2021-10-13 14:26:34 +0200

    Initial Commit

---
+++ calcul8/calcul8App.swift
@@ -0,0 +1,17 @@
+//
+//  calcul8App.swift
+//  calcul8
+//
+//  Created by Alex Szabó on 2021. 10. 13..
+//
+
+import SwiftUI
+
+@main
+struct calcul8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
